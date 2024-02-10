8b25781112d5e099f49b51a6304926ffed7c290c
thoriqqrn <thoriqym12@gmail.com>
2024-02-10 17:24:16 +0700

    first commit

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad
+        Copyright &copy; 2024 <div class="bullet"></div> Design By <a href="https://linkedin/in/thoriqqrn">Muhamad
             Nauval Azhar</a>
     </div>
     <div class="footer-right">
